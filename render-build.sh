7042d9bf0fb7169787dfaca7f97b14e9f922faba
NABA KRISHNA HAZARIKA <nabakrishna0111@gmail.com>
2025-03-14 01:48:02 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -4,5 +4,5 @@ apt-get update && apt-get install -y portaudio19-dev ffmpeg
 
 # Install Python dependencies
 pip install --upgrade pip
+pip install wheel setuptools
 pip install -r requirements.txt
-
